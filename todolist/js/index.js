68e5efa54198a2e89f12f39980b5dffc30d21702
trung <truongcongtrung2709@gmail.com>
2022-09-22 18:50:59 +0700

    upload Remove Task

--- todolist/js/index.js
+++ todolist/js/index.js
@@ -1,36 +1,50 @@
-let Tasks = [];
+let tasks = [];
 let todo = dom("#todo");
 let completed = dom("#completed");
 let newTask = dom("#taskName");
 
-let addTask = () => {
-  let newTaskVal = newTask.value;
-  let taskId = String(Date.now());
-  if (!newTaskVal) {
-    alert("Please enter a task name");
-    return;
+
+
+let addTask =(taskId,taskName) => {
+  if (!taskName) {
+       alert("Please enter a task name");
+       return;
+     }
+     const li = document.createElement("li");
+     li.setAttribute("task", taskId)
+     li.innerHTML = `
+       <p>${taskName}</p>
+       <i    class="fa-solid fa-check"></i>
+       <i data-id="${taskId}" class="fa-solid fa-check"></i>
+       `;
+     todo.appendChild(li);
+     tasks.push({taskId,taskName});
+     console.log(tasks);
+}
+let removeTask = (taskId) => {
+
+  var li = document.querySelector(`[task="${taskId}"]`);
+  
+
+  let index = tasks.indexOf(taskId);
+  tasks.splice(index,1);
+  console.log(tasks);
+  li.remove();
   }
-  const li = document.createElement("li");
-  li.innerHTML = `
-    <p>${newTaskVal}</p>
-    <i    class="fa-solid fa-check"></i>
-    <i  data-id="${taskId}" class="fa-solid fa-check"></i>
-    `;
-  todo.appendChild(li);
-};
 
-let removeTask = (id) => {
-  let li = dom(`[data-id="'${id}'"]`);
-  todo.removeChild(li);
-};
-todo.addEventListener("click", (e) => {
-  let id = e.target.getAttribute("data-id");
-  console.log(id);
-  if (!id) return;
-  removeTask(id);
-});
 
-//===============================
 function dom(selector) {
   return document.querySelector(selector);
 }
+//===============================
+dom("#addTask").addEventListener('click', e =>{
+  e.preventDefault();
+  let taskName = newTask.value;
+  let taskId = String(Date.now());
+  addTask(taskId, taskName); 
+});
+dom("#todo").addEventListener('click', e =>{
+  let id = e.target.getAttribute('data-id');
+  if(!id) return;
+  removeTask(id);
+});
